71d6c6c38bfeaa97e11714e06a29a965018ba744
alvarorrgg <alvaro.rodriguezr02@estudiante.uam.es>
2021-02-16 20:28:05 +0100

    Codigo ya creado con makefile bien y el codigo compila con algunos errores (falta revisar codigo y mejorarlo)

--- makefile
+++ makefile
@@ -1,14 +1,14 @@
-#makefile Ejercicio1
+#p1_e1
 
 p1_e1: vertex.o p1_e1.o
 	gcc -o p1_e1 vertex.o p1_e1.o
 
-p1_e1.o: p1_e1.c vertex.h 
-	gcc -Wall -pedantic -g p1_e1.c
-
-vertex.o: vertex.c vertex.h types.h
-	gcc -Wall -pedantic -g vertex.c
+p1_e1.o: p1_e1.c vertex.h types.h
+	gcc -Wall -c p1_e1.c
+	
+vertex.c: vertex.h types.h
+	gcc -Wall -c vertex.c
 
 clean:
-	@echo "Limpiando"
-	rm -rf *.o p1_e1
+	@echo Limpiando
+	rm -rf *.o ocaEXE
--- p1_e1.c
+++ p1_e1.c
@@ -1,37 +1,41 @@
 #include "vertex.h"
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
-	Vertex v1,v2;
+	Vertex *v1,*v2;
 	v1=vertex_init();
 	v2=vertex_init();
-	vertex_setId (*v1, 10);
-	vertex_setTag (*v1, "one");
-	vertex_setState (*v1, WHITE);
-	vertex_setId (*v2, 20);
-	vertex_setTag (*v2, "two");
-	vertex_setState (*v2, BLACK);
-	vertex_print(stdout,*v1);
-	vertex_print(stdout,*v2);
+	vertex_setId (v1, 10);
+	vertex_setTag (v1, "one");
+	vertex_setState (v1, WHITE);
+	vertex_setId (v2, 20);
+	vertex_setTag (v2, "two");
+	vertex_setState (v2, BLACK);
+	vertex_print(stdout,v1);
+	vertex_print(stdout,v2);
 	printf("\n");
-	if(vertex_cmp(*v1,*v2) == 0){
+	if(vertex_cmp(v1,v2) == 0){
 		printf("Equals? Yes\n");
 		}
 	else{
 		printf("Equals? No\n");
 	}
-	printf("%s",vertex_getTag(*v2);
-	Vertex* v3 = vertex_copy(*v1);
-	printf("%s",vertex_getId(*v3);
-	vertex_print(stdout,*v1);
-	vertex_print(stdout,*v3);
+	printf("%s",vertex_getTag(v2));
+	Vertex* aux_v1 = vertex_copy(v1);
+	printf("%li",vertex_getId(aux_v1));
+	vertex_print(stdout,v1);
+	vertex_print(stdout,aux_v1);
 	printf("\n");
-	if(vertex_cmp(*v1,*v2) == 0){
+	if(vertex_cmp(v1,v2) == 0){
 		printf("Equals? Yes\n");
 		}
 	else{
 		printf("Equals? No\n");
 	}
-	vertex_free(*v1);
-	vertex_free(*v2);
-	vertex_free(*v3);
-return 0;
+	vertex_free(v1);
+	vertex_free(v2);
+	vertex_free(aux_v1);
+	return 0;
 }
