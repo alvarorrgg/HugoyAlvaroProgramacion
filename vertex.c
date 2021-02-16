76f21dc6d6dfc42a210b254599ee69e1bdd535a3
torretass02 <huguitorres66@gmail.com>
2021-02-16 03:49:38 -0800

    Primeros cambios funciones

--- vertex.c
+++ vertex.c
@@ -1,11 +1,19 @@
 #include "vertex.h"
 #include <string.h>
-
+#define TAG_LENGTH 64
 
 /*----------------------------------------------------------------------------------------*/
 /*
 Private function:
 */
+
+struct _Vertex {
+long id;
+char tag[TAG_LENGTH];
+Label state;
+};
+
+
 Status vertex_setField (Vertex *v, char *key, char *value);
 
 Status vertex_setField (Vertex *v, char *key, char *value) {
@@ -22,7 +30,56 @@ Status vertex_setField (Vertex *v, char *key, char *value) {
   return ERROR;
 }
 
-/*----------------------------------------------------------------------------------------*/
+Vertex * vertex_init (){
+	malloc(sizeof(Vertex))
+}
+
+void vertex_free (void * v){
+	free(v);
+	}
+long vertex_getId (const Vertex * v){
+	if(v==NULL){
+	return -1;}
+	return v->id;
+	}
+const char* vertex_getTag (const Vertex * v){
+	if(v==NULL){
+	return NULL;}
+	return v->tag;
+	}
+Label vertex_getState (const Vertex * v){
+	if(v==NULL){
+	return ERROR_VERTEX;}
+	return v->state;
+	}
+Status vertex_setId (Vertex * v, const long id){
+	if(v==NULL || id<0){
+	return ERROR;}
+	v->id=id;
+	return OK;
+	}
+Status vertex_setTag (Vertex * v, const char * tag){
+	if(v==NULL || strlen(tag)>64){
+	return ERROR;}
+	v->tag=tag;
+	return OK;
+	}
+Status vertex_setState (Vertex * v, const Label state){
+	if(v==NULL){
+	return ERROR;}
+	v->state=state;
+	return OK;
+	}
+int vertex_cmp (const void * v1, const void * v2){
+	
+	}
+void * vertex_copy (const void * src){
+	
+	}
+int vertex_print (FILE * pf, const void * v){
+	
+	}
+
 Vertex *vertex_initFromString(char *descr){
   char buffer[1024];
   char *token;
